e736b55ab88eebf85540e186582a0f455e67fc67
348775859-wq <348775859@gapps.yrdsb.ca>
2026-04-27 22:38:33 -0400

    Update Banana.java

--- dodge-it-team-thivyan-dodgeit/Banana.java
+++ dodge-it-team-thivyan-dodgeit/Banana.java
@@ -14,6 +14,26 @@ public class Banana extends Actor
      */
     public void act()
     {
-        // Add your action code here.
+        move(-7);
+        
+        if(getX() <= 0) {
+            resetBanana();
+        }
+        
+        if(isTouching(Hero.class)){
+            SadFace sadFace = new SadFace();
+            getWorld().addObject(sadFace, 300, 200);
+            getWorld().removeObject(this);
+            
+        }
+    }
+    
+    public void resetBanana() {
+        int num = Greenfoot.getRandomNumber(2);
+        if (num == 0) {
+            setLocation(600, 100);
+        } else {
+            setLocation(600, 300);
+        }
     }
 }
